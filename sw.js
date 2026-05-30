50d29c9f68551e1ca750049f53ac32a054038160
ngovan960 <ngovan960@gmail.com>
2026-05-30 23:57:31 +0700

    Add Monetag ads script and update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11078683
+    "domain": "quge5.com",
+    "zoneId": 244570
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://quge5.com/act/files/service-worker.min.js?r=sw')
